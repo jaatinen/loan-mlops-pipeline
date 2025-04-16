4f33629662fa38ff24dc1bd17209cad5175ad293
Juho Jaatinen <juhojaatinen@Juho-MacBook-Air.local>
2025-04-16 22:16:55 +0300

    Fix CI test import path for mlflow_pipeline

--- mlflow_pipeline/model.py
+++ mlflow_pipeline/model.py
@@ -1,19 +1,33 @@
 import mlflow
 import mlflow.sklearn
+import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from mlflow_pipeline.data import load_data
 
 def train_model():
-    data = load_data()
-    X = data[['income', 'current_loan']]
-    y = data['label']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    # Dummy data with binary classification
+    X = np.array([
+        [3000, 1000],
+        [5000, 2000],
+        [7000, 3000],
+        [4000, 1500],
+        [2000, 800]
+    ])
+    y = np.array([1, 0, 1, 0, 1])
 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    # Log model and metrics to MLflow
     with mlflow.start_run():
-        model = LogisticRegression()
-        model.fit(X_train, y_train)
+        mlflow.log_param("model_type", "LogisticRegression")
+        mlflow.log_metric("accuracy", acc)
         mlflow.sklearn.log_model(model, "model")
 
-if __name__ == "__main__":
-    train_model()
+    return model
